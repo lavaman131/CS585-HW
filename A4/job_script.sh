2d2527b6ba25615ef81358840e6552424bcbe8fe
lavaman131 <lavaalex3@gmail.com>
2024-04-02 20:10:14 -0400

    improved model performance

--- A4/job_script.sh
+++ A4/job_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#$ -P dnn-motion                     # Specify the SCC project name you want to use
+#$ -P cs585                          # Specify the SCC project name you want to use
 #$ -l h_rt=24:00:00                  # Specify the hard time limit for the job
 #$ -l gpus=1                         # Specify the number of GPUs
 #$ -pe omp 4                         # Specify the number of cores
